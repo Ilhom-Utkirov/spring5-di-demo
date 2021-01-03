ef3500c296b4a273f541b8282b4838eef0a6ec49
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-03 15:32:14 +0500

    added Spring mutiple Environment Properties. Spring 5 case

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,16 +1,24 @@
 package guru.springframework.config;
 
 import guru.springframework.exmpleBeans.FakeDataSource;
+import guru.springframework.exmpleBeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//code below is working but it was introced in Spring 4
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+//In spring 5
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
      @Autowired
@@ -25,6 +33,25 @@ public class PropertyConfig {
 
     @Value("${guru.dburl}")
     String url;
+
+    @Value("${guru.jms.username}")
+    String username;
+
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+
+    @Value("${guru.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeJmsBroker jmsDataSources(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+
+        fakeJmsBroker.setUsername(username);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setPassword(jmsPassword);
+        return fakeJmsBroker;
+    }
 
     @Bean
     public FakeDataSource fakeDataSource(){
